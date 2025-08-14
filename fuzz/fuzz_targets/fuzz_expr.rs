7f15e25ee2ef68c611250e85090dcf715e55c1c7
Justin Whear <justin.whear@gmail.com>
2025-08-14 14:28:27 -0700

    remove unused constant

--- fuzz/fuzz_targets/fuzz_expr.rs
+++ fuzz/fuzz_targets/fuzz_expr.rs
@@ -1,8 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-const MAX_EXPR_LENGTH: usize = 10000;
-
 fuzz_target!(|data: &[u8]| {
     if let Ok(input) = std::str::from_utf8(data) {
         dbase_expr::fuzz_helper::translate_expr(&input);
