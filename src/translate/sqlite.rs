3556d785cb53de238a44e8ff232418ab64dac566
Justin Whear <justin.whear@gmail.com>
2026-08-10 10:28:32 -0700

    clean up lint

--- src/translate/sqlite.rs
+++ src/translate/sqlite.rs
@@ -7,8 +7,7 @@ use crate::{
         BinaryOp as TranslateBinaryOp, Error, ExpResult, Expression, FieldType, Parenthesize,
         SQLTree, TranslationContext, exps, ok,
         postgres::{
-            self, translate_args, translate_binary_op_right, translate_expr as default_translate,
-            wrong_type,
+            self, translate_binary_op_right, translate_expr as default_translate, wrong_type,
         },
     },
 };
